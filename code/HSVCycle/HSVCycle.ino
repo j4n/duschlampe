3e3d4cd29f8b8253a2c79c3477d0e667247b52e1
Jan <github@23t.de>
2013-11-11 15:27:23 -0500

    add rainbows!

---
+++ code/HSVCycle/HSVCycle.ino
@@ -0,0 +1,124 @@
+/* vim:ft=c:expandtab:nocindent:ts=2:sw=2
+HSV fade/bounce for Arduino - Stewart C. Russell - scruss.com - 2010/09/19
+
+Wiring:
+LED is RGB common cathode (SparkFun sku: COM-09264 or equivalent)
+    * Digital pin  9 → 165Ω resistor → LED Red pin
+    * Digital pin 10 → 100Ω resistor → LED Green pin
+    * Digital pin 11 → 100Ω resistor → LED Blue pin
+    * GND → LED common cathode.
+*/
+
+#define DELAY              2
+
+const int ledPins[3] = { 1, 0, 4 };
+
+long rgb[3];
+long rgbval, k;
+float hsv[3] = {
+  0.0, 0.5, 0.5
+};
+float hsv_min[3] = {
+  0.0, 0.0, 0.4 // keep V term greater than 0 for smoothness
+};
+float hsv_max[3] = {
+  6.0, 1.0, 1.0
+};
+float hsv_delta[3] = {
+  0.0005, 0.00013, 0.00011
+};
+
+/*
+chosen LED SparkFun sku: COM-09264
+ has Max Luminosity (RGB): (2800, 6500, 1200)mcd
+ so we normalize them all to 1200 mcd -
+ R  1200/2800  =  0.428571428571429   =   109/256
+ G  1200/6500  =  0.184615384615385   =    47/256
+ B  1200/1200  =  1.0                 =   256/256
+ */
+long bright[3] = {
+  109, 47, 256
+};
+
+void setup () {
+  randomSeed(analogRead(4));
+  for (k=0; k<3; k++) {
+    pinMode(ledPins[k], OUTPUT);
+    rgb[k]=0; // start with the LED off
+    analogWrite(ledPins[k], rgb[k] * bright[k]/256);
+    if (k>1 && random(100) > 50) {
+      // randomly twiddle direction of saturation and value increment on startup
+      hsv_delta[k] *= -1.0;
+    }
+  }
+}
+
+void loop() {
+  for (k=0; k<3; k++) { // for all three HSV values
+    hsv[k] += hsv_delta[k];
+    if (k<1) { // hue sweeps simply upwards
+      if (hsv[k] > hsv_max[k]) {
+        hsv[k]=hsv_min[k];
+      }    
+    }
+    else { // saturation or value bounce around
+      if (hsv[k] > hsv_max[k] || hsv[k] < hsv_min[k]) {
+        hsv_delta[k] *= -1.0;
+        hsv[k] += hsv_delta[k];
+      }
+    }
+    hsv[k] = constrain(hsv[k], hsv_min[k], hsv_max[k]); // keep values in range
+  }
+
+  rgbval=HSV_to_RGB(hsv[0], hsv[1], hsv[2]);
+  rgb[0] = (rgbval & 0x00FF0000) >> 16; // there must be better ways
+  rgb[1] = (rgbval & 0x0000FF00) >> 8;
+  rgb[2] = rgbval & 0x000000FF;
+
+  for (k=0; k<3; k++) { // for all three RGB values
+    analogWrite(ledPins[k], rgb[k] * bright[k]/256);
+  }
+  delay(DELAY);
+}
+
+long HSV_to_RGB( float h, float s, float v ) {
+  /*
+     modified from Alvy Ray Smith's site:
+   http://www.alvyray.com/Papers/hsv2rgb.htm
+   H is given on [0, 6]. S and V are given on [0, 1].
+   RGB is returned as a 24-bit long #rrggbb
+   */
+  int i;
+  float m, n, f;
+
+  // not very elegant way of dealing with out of range: return black
+  if ((s<0.0) || (s>1.0) || (v<0.0) || (v>1.0)) {
+    return 0L;
+  }
+
+  if ((h < 0.0) || (h > 6.0)) {
+    return long( v * 255 ) + long( v * 255 ) * 256 + long( v * 255 ) * 65536;
+  }
+  i = floor(h);
+  f = h - i;
+  if ( !(i&1) ) {
+    f = 1 - f; // if i is even
+  }
+  m = v * (1 - s);
+  n = v * (1 - s * f);
+  switch (i) {
+  case 6:
+  case 0: // RETURN_RGB(v, n, m)
+    return long(v * 255 ) * 65536 + long( n * 255 ) * 256 + long( m * 255);
+  case 1: // RETURN_RGB(n, v, m) 
+    return long(n * 255 ) * 65536 + long( v * 255 ) * 256 + long( m * 255);
+  case 2:  // RETURN_RGB(m, v, n)
+    return long(m * 255 ) * 65536 + long( v * 255 ) * 256 + long( n * 255);
+  case 3:  // RETURN_RGB(m, n, v)
+    return long(m * 255 ) * 65536 + long( n * 255 ) * 256 + long( v * 255);
+  case 4:  // RETURN_RGB(n, m, v)
+    return long(n * 255 ) * 65536 + long( m * 255 ) * 256 + long( v * 255);
+  case 5:  // RETURN_RGB(v, m, n)
+    return long(v * 255 ) * 65536 + long( m * 255 ) * 256 + long( n * 255);
+  }
+} 
---
+++ code/RainbowFade/RainbowFade.ino
@@ -0,0 +1,100 @@
+/* 
+ * vim:ft=c:expandtab:nocindent:ts=2:sw=2 
+ */
+
+#include <avr/pgmspace.h>
+
+//////////////////////////////////////////////////////
+// RainbowLED,                                      //
+// User Application,                                //
+// This example demonstrates how use analogWrite to //
+// control the brightness of an LED                 //
+//////////////////////////////////////////////////////
+
+// define LED pins
+#define LED_RED      1
+#define LED_GREEN    0
+#define LED_BLUE     4
+
+// A gradient for each channel
+PROGMEM  prog_uint8_t gradientRed [] = { 255,254,255,255,254,255,255,253,254,254,255,255,255,253,251,248,244,239,233,226,223,216,207,200,191,183,174,163,155,146,136,125,117,106,95,86,76,68,59,51,46,37,31,25,20,14,11,6,3,1,0,0,1,0,0,0,1,0,0,0,0,1,0,2,5,10,13,17,22,29,33,41,49,57,65,74,82,91,100,111,121,130,141,150,160,169,178,187,195,204,210,216,224,230,234,240,244,249,252,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,254,255,255,255,255,255,254,255};
+PROGMEM  prog_uint8_t gradientGreen [] = { 19,21,22,24,26,29,32,35,37,40,41,47,50,51,56,59,61,64,67,70,74,77,80,83,85,90,94,96,100,103,108,112,116,120,125,130,135,139,145,150,155,160,164,171,177,182,188,195,200,205,210,215,221,225,230,234,238,242,245,248,251,253,255,255,255,255,255,255,255,255,255,254,254,255,255,254,255,254,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,254,253,249,245,241,238,233,229,223,217,212,205,197,190,183,175,168,159,151,142,135,126,117,109,100,92,85,76,69,61,56,49,43,38};
+PROGMEM  prog_uint8_t gradientBlue []= { 67,76,85,94,103,113,123,132,142,152,162,171,181,189,197,205,213,219,225,232,236,242,245,250,253,255,255,254,254,255,255,254,254,255,255,255,255,255,255,254,255,254,255,255,254,255,255,253,255,255,255,254,255,255,254,255,255,253,250,247,242,238,233,226,220,212,205,196,189,179,168,161,149,139,129,121,109,99,89,80,72,60,54,46,39,32,25,20,17,12,6,4,3,1,0,0,0,0,0,0,0,0,0,0,1,0,0,2,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0};
+
+byte gradientIndex = 0;
+int singleDelay = 1;
+
+void setup(){
+  PWM4_init();
+
+  // setup LED pins
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+  // set LED pins high, all LEDs off
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_GREEN, LOW);
+  digitalWrite(LED_BLUE, LOW);
+}
+
+void loop(){
+  for (int red=0; red < 256; red ++){
+    analogWrite(LED_RED, red);
+    delay(singleDelay);
+  }
+
+  for (int blue=0; blue < 256; blue ++){
+    analogWrite4(blue);
+    delay(singleDelay);
+  }
+
+  for (int green=0; green < 256; green ++){
+    analogWrite(LED_GREEN, green);
+    delay(singleDelay);
+  }
+
+  for (int red=255; red >= 0; red --){
+    analogWrite(LED_RED, red);
+    delay(singleDelay);
+  }
+
+  for (int green=255; green >= 0; green --){
+    analogWrite(LED_GREEN, green);
+    delay(singleDelay);
+  }
+
+  for (int blue=255; blue >= 0; blue --){
+    analogWrite4(blue);
+    delay(singleDelay);
+  }
+ 
+  // Rainbow fade forever
+  while(true){
+    // Write inversed RGB-Values at index position
+    analogWrite(LED_RED, pgm_read_word_near(gradientRed + gradientIndex));
+    analogWrite(LED_GREEN, pgm_read_word_near(gradientGreen + gradientIndex));
+    analogWrite4(pgm_read_word_near(gradientBlue + gradientIndex));
+    // Increase index and handle overflow
+    gradientIndex ++;
+    if (gradientIndex > sizeof(gradientRed)-1){
+      gradientIndex = 0;
+    }
+    // Wait for a short time
+    delay(100);
+  }
+}
+
+
+void PWM4_init() {
+  // Set up PWM on Trinket GPIO #4 (PB4, pin 3) using Timer 1
+  TCCR1 = _BV (CS10);           // no prescaler
+  GTCCR = _BV (COM1B1) | _BV (PWM1B);  //  clear OC1B on compare
+  OCR1B = 127;                  // duty cycle initialize to 50%
+  OCR1C = 255;                  // frequency
+}
+ 
+// Function to allow analogWrite on Trinket GPIO #4 
+void analogWrite4(uint8_t duty_value) {  
+  OCR1B = duty_value;  // duty may be 0 to 255 (0 to 100%)
+}
+
